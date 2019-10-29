589fbeda9439da1d6275c4792276cccd5a82c84c
SireeR13 <sireereddy@outlook.com>
2019-10-29 22:58:00 +0000

    quiz 2

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,20 @@
+class ReviewsController < ApplicationController
+  def index
+    @review = Review.order("RANDOM()").first
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    Review.create(review_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def review_params
+    params.permit(:name, :comment)
+  end
+end
